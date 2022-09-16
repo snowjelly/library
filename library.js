0966e9a203c7302071530b2b3f170d42eca5728d
snowjelly <broadwellconnor@gmail.com>
2022-09-16 14:10:48 -0500

    book dom creation

--- library.js
+++ library.js
@@ -1,5 +1,5 @@
 let library = [];
-document.querySelector()
+const libraryDiv = document.querySelector('.library');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -27,9 +27,31 @@ function addBookToLibrary() {
 
   const newBookDiv = document.createElement('div');
   newBookDiv.setAttribute('class', 'book');
+  libraryDiv.appendChild(newBookDiv);
+
   const newBookTitle = document.createElement('h4');
   newBookTitle.setAttribute('class', 'book-title');
   newBookTitle.textContent = newBook.title;
+  newBookDiv.appendChild(newBookTitle);
+
+  const newBookAuthor = document.createElement('h5');
+  newBookAuthor.setAttribute('class', 'author');
+  newBookAuthor.textContent = newBook.author;
+  newBookDiv.appendChild(newBookAuthor);
+
+  const newBookPages = document.createElement('p');
+  newBookPages.setAttribute('class', 'pages');
+  newBookPages.textContent = newBook.pages;
+  newBookDiv.appendChild(newBookPages);
+
+  const newBookRead = document.createElement('p');
+  newBookRead.setAttribute('class', 'read');
+  newBookRead.textContent = newBook.read;
+  newBookDiv.appendChild(newBookRead);
+
+  
+  
+  
 
   library.push(newBook);
 }
